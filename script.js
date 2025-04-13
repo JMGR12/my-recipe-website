162f9fc427c2ecb7ddfd9c352ca8d4c3abf3dcd6
JMGR12 <jmg249@njit.com>
2025-04-12 23:35:16 -0400

    Added the the most recent files

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 console.log("JavaScript is linked! ✅");
+document.getElementById('searchInput').addEventListener('keyup', function () {
+    const filter = this.value.toLowerCase();
+    const cards = document.querySelectorAll('.recipe-card');
+  
+    cards.forEach(card => {
+      const title = card.querySelector('.card-title').textContent.toLowerCase();
+      const text = card.querySelector('.card-text').textContent.toLowerCase();
+      const match = title.includes(filter) || text.includes(filter);
+      card.style.display = match ? '' : 'none';
+    });
+  });
+  
